1686cbc8c6ae2f9ef5729f64b54f44b29180f83b
epetrenko <vechnii2007@gmail.com>
2026-08-19 22:57:21 +0200

    Импорт: универсальная чистка строк (эмодзи-маркеры, нумерация, любой ведущий мусор, хвостовые !!!)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "328561455" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3015435380" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
