97ba7d26ecc91eeecba36bd0b97edb3ae06a00ad
epetrenko <vechnii2007@gmail.com>
2026-08-19 11:50:57 +0200

    Приветственный онбординг: 5 шагов + Как это работает

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1554215780" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "963184876" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
