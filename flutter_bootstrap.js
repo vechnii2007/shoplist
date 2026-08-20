0764d34506b635dcf868a4e3520e428d4ffa7884
epetrenko <vechnii2007@gmail.com>
2026-08-20 10:59:46 +0200

    «Пропустить» в онбординге гасит обучение целиком: воронку и оба тура-коучмарки

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3974759350" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3348136659" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
